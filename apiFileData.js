546732ec22c5181e1f9fd2ed2a49cd7a6dae4e2e
Artiom Fotescu <artiomfotescu1@gmail.com>
2017-01-07 22:56:51 +0200

    Finished template for controller/model/router files

--- apiFileData.js
+++ apiFileData.js
@@ -6,36 +6,102 @@ const data = {};
 
 data.controller = (moduleName) => {
     const name = toUpper(moduleName);
-    return `import model from './model';
+    return `import Model from './model';
+import response from './../../../shared/response';
 
-class ${name}Ctrl {
+export default class ${name}Ctrl {
     constructor() {
     }
+    
+    static async getMany(ctx) {
+        const ${moduleName} = new Model(ctx.state.user);
+        const data = await ${moduleName}.getMany(ctx.query);
+        this.body = response(ctx.method, data.info, data.optional);
+    }
+    
+    static async getOne(ctx) {
+        const ${moduleName} = new Model(ctx.state.user);
+        const data = await ${moduleName}.getOne(ctx.query);
+        this.body = response(ctx.method, data);
+    }
+    
+    static async post(ctx) {
+        const ${moduleName} = new Model(ctx.state.user);
+        const data = await ${moduleName}.post(ctx.query, ctx.request.body);
+        this.body = response(ctx.method, data);
+    }
+    
+    static async put(ctx) {
+        const ${moduleName} = new Model(ctx.state.user);
+        const data = await ${moduleName}.put(ctx.query, ctx.request.body);
+        this.body = response(ctx.method, data);
+    }
+    
+    static async delete(ctx) {
+        const ${moduleName} = new Model(ctx.state.user);
+        await ${moduleName}.delete(ctx.query);
+        this.body = response(ctx.method);
+    }
 }
-
-export default ${name}Ctrl;
 `
 };
 
 data.model = (moduleName) => {
     const name = toUpper(moduleName);
-    return `import model from './model';
+    return `import path from 'path';
+import { sql, qFile, qPath } from './../../../shared/database';
 
-class ${name}Ctrl {
-    constructor() {
+export default class ${name}Ctrl {
+    constructor(user) {
+        this.user = user;
+    }
+    
+    async getMany(params) {
+        const limit = params.limit || 20;
+        const offset = params.offset || 0;
+        const queryParams = [limit, offset];
+        const data = { info: null, optional: { limit, offset }};
+        await sql.task(async (task) => {
+            const total = await task.one(qFile(qPath(sqlDirPath, 'getTotalCount')));
+            data.optional.total = total.count;
+            data.info = await task.any(qFile(qPath(sqlDirPath, 'getMany')));
+        });
+        return data;
+    }
+    
+    async getOne(params) {
+        return sql.one(qFile(qPath(sqlDirPath, 'getOne')));
+    }
+    
+    async post(params, body) {
+        return sql.one(qFile(qPath(sqlDirPath, 'post')));
+    }
+    
+    async put(params, body) {
+        return sql.one(qFile(qPath(sqlDirPath, 'put')));
+    }
+    
+    async delete(params) {
+        return sql.none(qFile(qPath(sqlDirPath, 'delete')))
     }
 }
-
-export default ${name}Ctrl;
 `
 };
 
 data.routes = (moduleName) => {
-  return `import Router from 'koa-router';
-import controller from './controller';
+    const name = toUpper(moduleName);
+    return `import Router from 'koa-router';
+import ${name} from './controller';
 
-const router = new Router();
-const url = '/${moduleName}';
+const router = new Router({
+    prefix: '/${moduleName}',
+});
+
+router.get('/', ${name}.getAll);
+router.post('/', ${name}.post);
+router.get('/:id', ${name}.getOne);
+router.update('/:id', ${name}.update);
+router.del('/:id', ${name}.delete);
 
 export default router;
 `;
